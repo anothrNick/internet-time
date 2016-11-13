b37fc44e11835ee9a0804bd41a9e77a7cccf46ad
Nick Sjostrom <nick.sjostrom12@gmail.com>
2016-11-13 13:42:01 -0500

    Show favicon. Sort times

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Internet time is a Chrome extension that shows how long you've spent at each website you visit
 
 Original extension template created with http://extensionizr.com/
+
+Icon made by Freepik from www.flaticon.com 
--- src/browser_action/scripts.js
+++ src/browser_action/scripts.js
@@ -2,12 +2,21 @@ chrome.storage.local.get('hosts', function(result){
     hosts = result.hosts;
     console.log(hosts);
 
-    for(var key in hosts) {
+    var array=[];
+	for(a in hosts){
+	 array.push([a,hosts[a]])
+	}
+	array.sort(function(a,b){return a[1].duration - b[1].duration});
+	array.reverse();
+
+	for (var i = 0; i < array.length; i++) {
+		var key = array[i][0];
+		var host = array[i][1];
     	if(hosts[key].duration) {
-	    	var html = createRow(key, hosts[key]);
+	    	var html = createRow(key, host);
 	    	document.getElementById("table").innerHTML = document.getElementById("table").innerHTML + html;
-	    }
-    }
+    	}
+	}
 });
 
 function createRow(key, host) {
@@ -15,7 +24,8 @@ function createRow(key, host) {
 	var start = moment(now);
 	var end   = moment(now - host.duration);
 	return "<tr class='host-row'>" +
-				"<td><!--<img src="+host.favicon+" />-->"+key+"</td>"+
+				"<td><img src='https://www.google.com/s2/favicons?domain="+key+"' /></td>"+
+				"<td>"+key+"</td>"+
 				"<td style='text-align:right;'>"+ end.from(start, true) +"</td>"
 			"</tr>";
 }
