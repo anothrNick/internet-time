8dd657e92c19d83022f0da1def2f8169ca663f06
Nick Sjostrom <nick.sjostrom12@gmail.com>
2016-11-13 00:21:51 -0500

    use moment to dsplay humanized time

--- src/browser_action/scripts.js
+++ src/browser_action/scripts.js
@@ -3,7 +3,19 @@ chrome.storage.local.get('hosts', function(result){
     console.log(hosts);
 
     for(var key in hosts) {
-    	var html = "<tr><td><!--<img src="+hosts[key].favicon+" />-->"+key+"</td><td>"+hosts[key].duration+"</td><td>"+hosts[key].count+"</td></tr>"
-    	document.getElementById("table").innerHTML = document.getElementById("table").innerHTML + html;
+    	if(hosts[key].duration) {
+	    	var html = createRow(key, hosts[key]);
+	    	document.getElementById("table").innerHTML = document.getElementById("table").innerHTML + html;
+	    }
     }
 });
+
+function createRow(key, host) {
+	var now = new Date().getTime();
+	var start = moment(now);
+	var end   = moment(now - host.duration);
+	return "<tr class='host-row'>" +
+				"<td><!--<img src="+host.favicon+" />-->"+key+"</td>"+
+				"<td style='text-align:right;'>"+ end.from(start, true) +"</td>"
+			"</tr>";
+}
